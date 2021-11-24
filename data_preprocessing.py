1f98e54f4ce22842582fd3224caf57c53db908e5
jaehoo98 <51286116+jaehoo98@users.noreply.github.com>
2021-11-24 22:49:15 +0900

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,13 +2,52 @@ from typing import List, Dict
 import pandas as pd
 
 class get_data():
-    def __init__(self, raw_data_path="ml-100k"):
+    def __init__(self):
         u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-        self.user = pd.read_csv(f"{raw_data_path}/u.user", sep='|', names=u_cols, encoding='latin-1')
+        self.user = pd.read_csv('data/raw/u.user',
+                                sep='|',
+                                names=u_cols,
+                                usecols=["user_id", "age", "sex", "occupation"],
+                                encoding='latin-1')
+
         r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-        self.rating = pd.read_csv(f"{raw_data_path}/u.data", sep='\t', names=r_cols, encoding='latin-1')
-        m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
-        self.movie = pd.read_csv(f"{raw_data_path}/u.item", sep='|', names=m_cols, usecols=range(5), encoding='latin-1')
+        self.rating = pd.read_csv('data/raw/u.data',
+                                  sep='\t',
+                                  names=r_cols,
+                                  usecols=["user_id", "movie_id", "rating"],
+                                  encoding='latin-1')
+        m_cols = [
+            "movie_id",
+            "title",
+            "release_date",
+            "video_release_date",
+            "imdb_url",
+            # features from here:
+            "unknown",
+            "action",
+            "adventure",
+            "animation",
+            "childrens",
+            "comedy",
+            "crime",
+            "documentary",
+            "drama",
+            "fantasy",
+            "film_noir",
+            "horror",
+            "musical",
+            "mystery",
+            "romance",
+            "sci_fi",
+            "thriller",
+            "war",
+            "western",
+        ]
+        self.movie = pd.read_csv('data/raw/u.item',
+                                 sep='|',
+                                 names=m_cols,
+                                 usecols=["movie_id"] + m_cols[5:],
+                                 encoding='latin-1')
         self.movie_rating = pd.merge(self.movie, self.rating)
         self.lens = pd.merge(self.movie_rating, self.user)
 
