5cb1e4903fd99505bcc360cb2e2b7735087642b1
jaehoo98 <51286116+jaehoo98@users.noreply.github.com>
2021-11-24 22:50:24 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,6 +1,47 @@
 import pandas as pd
 import dgl
 import torch
+import stellargraph as sg
+from data_preprocessing import get_data
+from feature_matrix_generation import get_users_features_matrix, get_items_features_matrix
+from sklearn import preprocessing
+
+def generate_stellar_graph():
+    df = get_data()
+    users = df.users()
+    movies = df.movies()
+    ratings = df.ratings()
+    movies = movies[movies["movie_id"] != 267]
+    ratings = ratings[ratings["movie_id"] != 267]
+
+    users_ids = "user_" + users["user_id"].astype(str)
+    movies["movie_id"] = "movie_" + movies["movie_id"].astype(str)
+    movies.set_index("movie_id", inplace = True)
+    ratings["user_id"] = "user_" + ratings["user_id"].astype(str)
+    ratings["movie_id"] = "movie_" + ratings["movie_id"].astype(str)
+
+    ########## add encoded features to user and movie data
+
+    user_feat = get_users_features_matrix('data/raw/u.user')
+    movie_feat = get_items_features_matrix('data/raw/u.item')
+    encoded_movies = movies.assign(release_date = movie_feat[:,0])
+
+    feature_encoding = preprocessing.OneHotEncoder(sparse = False)
+    onehot = feature_encoding.fit_transform(users[["sex", "occupation"]])
+    scaled_age = preprocessing.scale(users["age"])
+    encoded_users = pd.DataFrame(onehot, index = users_ids).assign(scaled_age = scaled_age)
+
+    #print("\n\n-------- encoded_users --------\n", encoded_users)
+    #print("\n\n-------- encoded_movies --------\n", encoded_movies)
+    #print("\n\n-------- ratings --------\n", ratings)
+    g = sg.StellarGraph(
+        {"user" : encoded_users, "movie" : encoded_movies},
+        {"rating" : ratings[["user_id", "movie_id"]]},
+        source_column = "user_id",
+        target_column = "movie_id",
+    )
+    return g, ratings
+
 
 def generate_dgl_graph(data: pd.DataFrame):
     # We generate a graph with "user_id" nodes as source, edge types "rating" and
